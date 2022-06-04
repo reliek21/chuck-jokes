c6b0fb6d6ccae152d1b8614a14d09844477c4183
reliek21 <keilerguardo@gmail.com>
2022-06-04 14:39:09 -0500

    Improve folder structure

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        padding: '2rem',
+      },
+    }
+  },
+  plugins: [],
+}
